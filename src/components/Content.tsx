8f45ccbfa7a5736ba1ac3c7ca083dc39558a9251
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-03-15 21:27:02 -0400

    feat: ensure content component shows movies on select genres

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../Models/MovieModel";
+import { MovieCard } from './MovieCard';
+import { Header } from './Header'
+interface ContentProps {
+  movies: MovieProps[],
+  selectedGenreTitle: string
+}
+
+export function Content(props: ContentProps) {
+  const {movies, selectedGenreTitle} = props
+  
+  return (
+    <div className="container">
+    <Header genreTitle={selectedGenreTitle}/>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
